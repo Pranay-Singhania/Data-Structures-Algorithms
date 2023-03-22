410fbfcd1b6de7b953d164ba6fe71273250d1d85
Pranay Singhania <pranay.s4299@gmail.com>
2023-03-23 00:01:14 +0530

    edit sum-zero

--- sum-zero.js
+++ sum-zero.js
@@ -1,4 +1,23 @@
-// Video 1
+// 1st video
+// Checking sum 0
+// [-5,-4,-3,-2,0,2,4,6,8] -> input
+// [? ?] -> output
+// Time complexity O(n^2)
+
+let a = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+
+const sumSlow = (b) => {
+  for (let i of b) for (let j of b) if (i + j === 0) return [i, j];
+};
+console.log(sumSlow(a));
+
+// -----------------------------------------------------------------------------------------------------
+
+// 2nd video
+// Checking sum 0
+// [-5,-4,-3,-2,0,2,4,6,8] -> input
+// [? ?] -> output
+// Time complexity O(n)
 
 const sumFast = (b) => {
   li = 0;
