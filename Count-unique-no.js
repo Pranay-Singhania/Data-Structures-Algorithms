0737e2f7e6fca5ba0883b08f8241c3dd0db0308f
Pranay Singhania <pranay.s4299@gmail.com>
2023-03-23 23:52:55 +0530

    editted count-unique-no

--- Count-unique-no.js
+++ Count-unique-no.js
@@ -1,7 +1,7 @@
 // 4th video
 // Count unique numbers
 // [1,1,2,2,3,4,4,5,6,7,8,8] -> input
-// 1,2,3,4,5,6,7,8 -> output
+// 8 -> output
 // Time complexity O(n^2)
 
 // const printUniqueElements=(a)=>{
@@ -10,18 +10,20 @@
 //     for(let i in count) console.log(i);
 // }
 
-const printUniqueElements=(a)=>{
-    let i=0;
-    for(j=1;j<a.length-1;j++)
-        if(a[i]===a[j]) return;
-        else {
-            i+=1;
-            a[i]=a[j];
-        }
-        console.log(a[i])
-    }
+const printUniqueElements = (a) => {
+  if (a.length > 0) {
+    let i = 0;
+    for (j = 1; j < a.length; j++)
+      if (a[i] !== a[j]) {
+        i++;
+        a[i] = a[j];
+      }
+    return i + 1;
+  } else {
+    console.log("array is empty");
+  }
+};
 
-printUniqueElements([1,1,2,2,3,4,4,6,7,8,8])
-
+console.log(printUniqueElements([1, 1, 2, 2, 3, 4, 4, 6, 7, 8, 8]));
 
 // ---------------------------------------------------------------------------------------------------
